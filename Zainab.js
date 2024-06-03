c89908327f7a4179670d0eb9b3a5b67360121104
Omotola Sanni <omotola302@gmail.com>
2024-06-03 09:30:45 +0100

    first commit

---
+++ Zainab.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    // Prevents the default form submission
+    alert('Form submitted successfully!');
+});
